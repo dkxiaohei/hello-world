ae84d14ec9f68c6c4eedca5da83012ae8f8bce61
dkxiaohei <dkxiaohei@outlook.com>
2022-03-28 23:39:35 +0800

    guess-number-rust: replace 'loop' with 'while'

--- guess-number/guess-number-rust/src/main.rs
+++ guess-number/guess-number-rust/src/main.rs
@@ -4,20 +4,18 @@ use std::cmp::Ordering;
 
 fn main() {
     let n = 100;
-    guess(n);
+    let m = 10;
+    guess(n, m);
 }
 
-fn guess(n: u32) {
+fn guess(n: u32, m: u32) {
     println!("Guess a number between 1 and {}", n);
 
     let number = rand::thread_rng().gen_range(1..n + 1);
     let mut attempt = 0;
 
-    loop {
+    while attempt < m {
         attempt += 1;
-        if attempt > 10 {
-            break;
-        }
 
         let mut guess = String::new();
         println!("Please type in your guess");
@@ -40,5 +38,5 @@ fn guess(n: u32) {
 
     }
 
-    println!("Oops! You lose");
+    println!("Oops! You lose after {} attempts", m);
 }
