f23dbb57dca87176d721f222bb406412d4bb3c70
kravcha <coderkravcha@gmail.com>
2022-04-07 23:58:43 +0800

    add asm hello world

---
+++ hello-world/hello.asm
@@ -0,0 +1,18 @@
+format ELF64
+public _start
+
+msg db "hello, world", 0
+len = $-msg
+
+_start:
+	mov rax, 4
+	mov rbx, 1
+	mov rcx, msg
+	mov rdx, len
+	int 0x80
+	call exit
+
+exit:
+	mov rax, 1
+	mov rbx, 0
+	int 0x80
