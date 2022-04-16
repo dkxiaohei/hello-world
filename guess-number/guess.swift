6f93dbc3b140bdc484c115754836f5e509b09d47
dkxiaohei <dkxiaohei@outlook.com>
2022-04-16 23:03:17 +0800

    Handle errors in guess.swift

--- guess-number/guess.swift
+++ guess-number/guess.swift
@@ -3,7 +3,6 @@ guess(n: n)
 
 func guess(n: Int) {
     let number = Int.random(in: 1...n)
-    var guessStr: String?
     var attempt = 0
 
     print("Guess a number between 1 and \(n)")
@@ -12,16 +11,21 @@ func guess(n: Int) {
         attempt += 1
 
         print("Please type in your guess:", terminator: " ")
-        guessStr = readLine()
-        let guess = Int(guessStr!)!
-
-        if guess < number {
-            print("Higher number please")
-        } else if guess > number {
-            print("Lower number please")
+        if let guessStr = readLine() {
+            if let guess = Int(guessStr) {
+                if guess < number {
+                    print("Higher number please")
+                } else if guess > number {
+                    print("Lower number please")
+                } else {
+                    print("Bingo! You guessed the number in \(attempt) attempts")
+                    return
+                }
+            } else {
+                continue
+            }
         } else {
-            print("Bingo! You guessed the number in \(attempt) attempts")
-            return
+            continue
         }
     }
 
